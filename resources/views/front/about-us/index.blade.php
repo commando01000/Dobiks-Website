f0bb65562c63e57f8a4f8cc6dd09ee1810bf0581
Mohamed Salama Mousa <91347929+MohamadSalamaMouse@users.noreply.github.com>
2024-07-14 13:05:44 +0300

    test

--- resources/views/front/about-us/index.blade.php
+++ resources/views/front/about-us/index.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.front.app')
 
 @section('content')
@@ -76,11 +75,12 @@
             </section>
 
             <section class="carousel-section mt-5 pt-5 pb-5 position-relative">
-                @foreach ($testimonials as $testimonial)
-                    <div class="container">
-                        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
+
+                <div class="container">
+                    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            @foreach ($testimonials as $testimonial)
+                                <div class="carousel-item @if ($loop->first) active @endif">
                                     <div class="row gx-5 align-items-center w-100">
                                         <div class="col-md-4">
                                             {{-- <img src="{{ asset('assets/front_assets/images/so3ody.jpeg') }}"
@@ -148,79 +148,13 @@
                                                 class="d-inline-block content-section__description ui text size-textmd">
                                                 {{ $testimonial->title }}
                                             </p>
-
-
                                         </div>
                                     </div>
                                 </div>
-                                <div class="carousel-item">
-                                    <div class="row gx-5 align-items-center w-100">
-                                        <div class="col-md-4">
-                                            <img src="{{ asset('assets/front_assets/images/so3ody.jpeg') }}"
-                                                class="w-100 object-fit-cover" style="min-height: 400px;" alt="so3ody">
-                                        </div>
-                                        <div class="col-md-8 mt-5 pt-5">
-                                            <div
-                                                class="carousel-header w-100 pb-5 d-flex flex-row align-items-center justify-content-between">
-                                                <div>
-                                                    <div class="quote-container">
-                                                        <span class="quote"></span>
-                                                        {{-- before --}}
-                                                    </div>
-                                                </div>
-                                                <div class="d-flex flex-row">
-                                                    <div class="pe-5">
-                                                        <button style="width: 80px; height: 80px"
-                                                            class="slider-navigation__icon--prev carousel-control-prev"
-                                                            type="button" data-bs-slide="prev"
-                                                            data-bs-target="#carouselExampleAutoplaying">
-                                                            <img
-                                                                src="{{ asset('assets/front_assets/images/img_arrow_left.svg') }}">
-                                                        </button>
-                                                    </div>
-                                                    <div>
-                                                        <button style="width: 80px; height: 80px"
-                                                            class="slider-navigation__icon--next carousel-control-next"
-                                                            type="button" data-bs-slide="next"
-                                                            data-bs-target="#carouselExampleAutoplaying">
-                                                            <img
-                                                                src="{{ asset('assets/front_assets/images/img_arrow_right_blue_gray_100.svg') }}">
-                                                        </button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <p class="content-section__description">An interior design agency can create
-                                                content
-                                                that showcases its work, highlights
-                                                design <br> trends, and provides educational resources for clients and
-                                                followers.
-                                                Some
-                                                potential content <br> ideas for an interior design agency An interior
-                                                design
-                                                agency
-                                                can
-                                                create content that <br> showcases its work, highlights design trends, and
-                                                provides
-                                                educational resources for clients <br> and followers. Some potential content
-                                                ideas
-                                            </p>
-                                            <div style="width: 40px" class="mb-3 content-section__divider"></div>
-                                            <span
-                                                class="d-inline-block content-section__description ui text size-textmd fs-5">Ahmed
-                                                Bakry</span>
-                                            <br>
-                                            <p style="font-size: 15px"
-                                                class="d-inline-block content-section__description ui text size-textmd">
-                                                From
-                                                USA
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            @endforeach
                         </div>
                     </div>
-                @endforeach
+                </div>
             </section>
             <section class="our-trusted-clients p-5 m-5">
                 <div class="container">
