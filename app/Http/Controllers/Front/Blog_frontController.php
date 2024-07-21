34a54cb6c9fce6a80b20bf509acd9c28b6806ffd
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-07-21 14:53:30 +0300

    .

--- app/Http/Controllers/Front/Blog_frontController.php
+++ app/Http/Controllers/Front/Blog_frontController.php
@@ -26,7 +26,7 @@ class Blog_frontController extends Controller
 
         $blog = Blog::where('slug', $slug)->first();
 
-        if (!$blog) {
+        if (!$blog) {asd
             abort(404);
         }
 
