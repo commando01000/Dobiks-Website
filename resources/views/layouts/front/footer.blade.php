f141a92c608de9e9b9f149a8a108ae5fd90b5969
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-07-21 16:04:11 +0300

    footer

--- resources/views/layouts/front/footer.blade.php
+++ resources/views/layouts/front/footer.blade.php
@@ -67,12 +67,15 @@
                                 {{-- <span style="line-height: 30px" class="content-section__description fs-6">Social Share :</span> --}}
                                 <a href="{{ Utility::getsettings('contact_facebook') }}"
                                     class="footer__social-button--facebook">
-                                    <img src="http://127.0.0.1:8000/assets/front_assets/images/img_facebook_app_symbol.svg">
+                                    <img
+                                        src="http://127.0.0.1:8000/assets/front_assets/images/img_facebook_app_symbol.svg">
                                 </a>
-                                <a href="{{ Utility::getsettings('contact_instagram') }}" class="footer__link-button--link">
+                                <a href="{{ Utility::getsettings('contact_instagram') }}"
+                                    class="footer__link-button--link">
                                     <img src="http://127.0.0.1:8000/assets/front_assets/images/img_link.svg">
                                 </a>
-                                <a href="{{ Utility::getsettings('contact_twitter') }}" class="footer__link-button--link">
+                                <a href="{{ Utility::getsettings('contact_twitter') }}"
+                                    class="footer__link-button--link">
                                     <img src="http://127.0.0.1:8000/assets/front_assets/images/img_trash.svg">
                                 </a>
                             </div>
@@ -212,4 +215,3 @@
 </script>
 <script src="https://cdn-script.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="{{ asset('assets/front_assets/assets/js/home.js') }}?v=<?= time() ?>"></script>
-
