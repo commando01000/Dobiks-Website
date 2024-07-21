2779f2d79164d19bef72b0e3a927478b651de85a
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-07-21 15:56:52 +0300

    Update index.blade.php

--- resources/views/front/about-us/index.blade.php
+++ resources/views/front/about-us/index.blade.php
@@ -105,7 +105,8 @@
                             <div class="col-md-3">
                                 <div class="content-section__column--secondary">
                                     <p class="content-section__button pt-2 ui text size-btn_text">
-                                        <a class="text-decoration-none text-white" href="{{ route('contact') }}">CONTACT US</a>
+                                        <a class="text-decoration-none text-white" href="{{ route('contact') }}">CONTACT
+                                            US</a>
                                     </p>
                                 </div>
                             </div>
