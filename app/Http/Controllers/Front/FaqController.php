5bb9a66a97da467be081d0577745ae1ec29b0c43
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-07-24 14:28:15 +0300

    faq_accordion

--- app/Http/Controllers/Front/FaqController.php
+++ app/Http/Controllers/Front/FaqController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use App\Facades\UtilityFacades;
 use App\Models\Faq;
-
+use Illuminate\Support\Facades\App;
 
 class FaqController extends Controller
 {
     public function index()
     {
         $lang = UtilityFacades::getActiveLanguage();
-        \App::setLocale($lang);
+        App::setLocale($lang);
         $faqs = Faq::orderBy('order')->get();
         return view('front/faqs.index', compact('lang', 'faqs'));
     }
---
+++ resources/views/front/faqs/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.front.app')
+
+
+@section('content')
+    <section id="faq" class="p-1 min-vh-100">
+        <div class="container-fluid p-0 m-0 overflow-hidden">
+            <div class="section__header justify-content-center align-items-center">
+                <h1 class="content-section__title ui heading size-heading_1">
+                    Faq
+                </h1>
+                <small class="content-section__description fs-6">HOME / FAQ</small>
+            </div>
+            <div class="faq-accordion w-100 pb-5 min-vh-100" style="background: #0E0E0EE5 !important;">
+                <div class="row w-50 m-auto mt-5 pt-5">
+                    <div class="col-md-12 col-lg-12  mb-5 mb-md-0">
+                        <div class="accordion custom-accordion-style-1 appear-animation animated fadeInUpShorter appear-animation-visible"
+                            data-appear-animation="fadeInUpShorter" data-appear-animation-delay="250" id="accordion1"
+                            style="animation-delay: 250ms;">
+                            @foreach ($faqs as $key => $item)
+                                <div class="accordion-item" style="background: #1C1C1C;">
+                                    <h2 class="accordion-header w-100" id="heading{{ $key }}">
+                                        <button style="width: 100% !important"
+                                            class="accordion-button{{ $loop->first ? '' : ' collapsed' }} text-white"
+                                            type="button" data-bs-toggle="collapse"
+                                            data-bs-target="#collapse{{ $key }}"
+                                            aria-expanded="{{ $loop->first ? 'true' : 'false' }}"
+                                            aria-controls="collapse{{ $key }}">
+                                            {{ $loop->iteration }} . {{ $item['questions'] }}
+                                        </button>
+
+                                    </h2>
+                                    <div id="collapse{{ $key }}"
+                                        class="accordion-collapse collapse{{ $loop->first ? ' show' : '' }}"
+                                        aria-labelledby="heading{{ $key }}"
+                                        data-bs-parent="#accordionPanelsStayOpenExample">
+                                        <div class="accordion-body text-white">
+                                            {{ $item['answer'] }}
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr>
+                            @endforeach
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
