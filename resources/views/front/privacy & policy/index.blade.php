13506ff8bf97ac20886fbe553eeedc4c90ff8706
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-07-27 15:06:18 +0300

    privacy policy

---
+++ resources/views/front/privacy & policy/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.front.app')
+
+@section('title', 'Projects')
+
+
+@section('content')
+    <section id="privacy-policy" class="w-100 mt-0 p-1">
+        <main class="container-fluid ps-0 pe-0">
+            <div class="section__header pt-3 pb-1 justify-content-center align-items-center">
+                <h1 class="content-section__title ui heading size-heading_1">
+                    Privacy & Policy
+                </h1>
+                <small class="content-section__description fs-6">HOME / PRIVACY & POLICY</small>
+            </div>
+            <div style="background-color: #191919;max-width: 100% !important" id="privacy-policy-content"
+                class="privacy-policy w-100 mt-5 pt-5 pb-5 p-1 overflow-hidden">
+
+            </div>
+        </main>
+    </section>
