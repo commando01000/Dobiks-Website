3dee1a948e4a80faced4e9ee1d75e910456f15dd
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-08-09 16:29:04 +0300

    z-index

--- resources/views/layouts/front/app.blade.php
+++ resources/views/layouts/front/app.blade.php
@@ -4,6 +4,7 @@
 @yield('content')
 @include('layouts.front.footer')
 @yield('style')
+@yield('js')
 <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
 <script src="{{ asset('assets/js/popper.min.js') }}"></script>
 <script src="{{ asset('assets/js/jquery-3.3.1.min.js') }}"></script>
